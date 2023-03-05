d6c1fff959382f80de56ed7b20001fe287346e9f
shivangi4742 <neural.shivangi@gmail.com>
2023-03-05 22:07:42 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,26 +1,26 @@
 from collections import defaultdict
 
 # Read the input file and group the sentences by their prefix
-sentences_by_prefix = defaultdict(list)
+_sentencesByPrfix = defaultdict(list)
 with open("input.txt", "r") as input_file:
-    for line in input_file:
-        prefix, sentence = line.strip().split(" ", 1)
-        sentences_by_prefix[prefix].append(sentence)
+    for _line in input_file:
+        _prefix, _sentence = _line.strip().split(" ", 1)
+        _sentencesByPrfix[_prefix].append(_sentence)
 
 # Find groups of similar sentences and extract the changing word
 
 groups = []
-for prefix, sentences in sentences_by_prefix.items():
+for _prefix, _sentences in _sentencesByPrfix.items():
     groups_by_sentence = defaultdict(list)
     # print(sentences)
-    for sentence in sentences:
-        prefix, pattern = sentence.split(" ", 1)
-        groups_by_sentence[pattern].append(prefix)
+    for sentence in _sentences:
+        _prefix, _pattern = sentence.split(" ", 1)
+        groups_by_sentence[_pattern].append(_prefix)
 
-    for pattern, prefixes in groups_by_sentence.items():
-        if len(prefixes) >=1:
-            changing_word = pattern.split()
-            groups.append((prefixes, pattern, changing_word))
+    for _pattern, _prefixes in groups_by_sentence.items():
+        if len(_prefixes) >=1:
+            _changing_word = _pattern.split()
+            groups.append((_prefixes, _pattern, _changing_word))
 
 
 # Output the results to a file

05d4645e9d93f6b8beb57afe90ab483edae185bb
shivangi4742 <neural.shivangi@gmail.com>
2023-03-08 17:29:55 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,50 +1,44 @@
+key = [" into the ", " at a " ]
+
+original_sentence= []
 def parse_input_file(file_path):
     sentences = []
     with open(file_path, "r") as f:
         for line in f:
+            original_sentence.append(line)
             date , timestamp, sentence = line.strip().split(" ", 2)
             name, actions = sentence.split(" is ")
-            action, actioner = actions.split(" into the ")
+            half_prefix = actions.split(key[0])
+            if(len(half_prefix) > 1):
+                action, actioner = actions.split(key[0])
+            else:
+                action, actioner = actions.split(key[1])
+            
             sentences.append({"timestamp": date + timestamp, "name": name, "action": action, "actioner": actioner})
     return sentences
 
-def group_sentences_by_name(sentences):
-    name = {}
-    for sentence in sentences:
-        if sentence["name"] in name:
-            name[sentence["name"]].append({"action": sentence["action"], "timestamp": sentence["timestamp"]})
-        else:
-            name[sentence["name"]] = [{"action": sentence["action"], "timestamp": sentence["timestamp"]}]
-    return name
-
-def group_sentences_by_values(sentences):
-    v = {}
-    for sentence in sentences:
-        if sentence["actioner"] in v:
-            v[sentence["actioner"]].append({'name' : sentence["name"],"action": sentence["action"], "timestamp": sentence["timestamp"]})
-        else:
-            v[sentence["actioner"]] = [{'name' : sentence["name"],"action": sentence["action"], "timestamp": sentence["timestamp"]}]
-    return v
 
 
-def group_sentences_by_action(sentences):
+def group_sentences_by_action(sentences, k):
     actions = {}
     for sentence in sentences:
-        if sentence["action"] in actions:
-            actions[sentence["action"]].append({"name": sentence["name"], "timestamp": sentence["timestamp"]})
+        if sentence[k] in actions:
+            actions[sentence[k]].append({ "timestamp": sentence["timestamp"]})
         else:
-            actions[sentence["action"]] = [{"name": sentence["name"], "timestamp": sentence["timestamp"]}]
+            actions[sentence[k]] = [{ "timestamp": sentence["timestamp"]}]
     return actions
 
 
 sentences = parse_input_file("input.txt")
-name = group_sentences_by_name(sentences)
-actions = group_sentences_by_action(sentences)
-actioners  = group_sentences_by_values(sentences)
+name = group_sentences_by_action(sentences, 'name')
+actions = group_sentences_by_action(sentences, 'action')
+actioners  = group_sentences_by_action(sentences, 'actioner')
+
+
 
 with open("output.txt", "w") as f:
-    for sentence in sentences:
-        f.write(sentence["timestamp"] + " " + sentence["name"]  + " " + sentence["action"] + " " +sentence["actioner"]+ "\n")
+    for sentence in original_sentence:
+        f.write(f"{sentence}  \n")
     if (len(name.keys())> 1):
         f.write(f"The changing words was: {', '.join(sorted(name))}\n")
     elif (len(actions.keys())>1):
